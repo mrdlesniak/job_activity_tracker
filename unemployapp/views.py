85e8a21e296dd40e973748f35c306e2f3ac850df
Dylan <mrdlesniak@gmail.com>
2020-05-18 16:21:34 -0700

    added more information to 'application' accordion. Reversed date order so most recent dates are near the top.

--- unemployapp/views.py
+++ unemployapp/views.py
@@ -5,7 +5,7 @@ import datetime
 
 # Create your views here.
 def index(request):
-    all_dates = Date.objects.order_by("date")
+    all_dates = Date.objects.order_by("-date")
     all_applications = Application.objects.order_by("company_name")
     all_activities = Activity.objects.all()
     
